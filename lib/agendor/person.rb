331c853d10afd809ae4d3e6ceedd25262fa99075
Nathan Thiesen <nathan.thiesen@outlook.com>
2014-12-31 12:10:00 -0200

    person is done

--- lib/agendor/person.rb
+++ lib/agendor/person.rb
@@ -1,20 +1,28 @@
 module Agendor
 
   class Person < Agendor::Base
-    
-    def process_hash(params)
-	{
-	  "name" => params[:name],
-	  "role" => params[:role],
-	  "emails" => params[:emails_array],
-	  "description" => params[:description]
-	}
-	end
 
-   	def resource_path
-  	  "#{api_path}/people"
-	end
+    def hash_keys
+    [
+      :personId,
+      :category,
+      :organization,
+      :phones,
+      :emails,
+      :social,
+      :address,
+      :userOwner,
+      :name,
+      :role,
+      :ranking,
+      :cpf
+    ]
+    end
 
-  end
+   def resource_path
+     "#{api_path}/people"
+   end
+
+ end
 
 end
