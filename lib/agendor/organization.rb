9485cf48d34b275afded7c6b2a7014a1006c34c6
Nathan Thiesen <nathan.thiesen@outlook.com>
2014-12-31 12:11:50 -0200

    change gem version and finalize organization

--- lib/agendor/organization.rb
+++ lib/agendor/organization.rb
@@ -1,19 +1,25 @@
 module Agendor
 
   class Organization < Agendor::Base
-    
-    def process_hash(params)
-	{
-	  "nickname" => params[:name],
-	  "description" => params[:description],
-	  "website" => params[:website],
-	}
-	end
 
-   	def resource_path
-  	  "#{api_path}/organizations"
-	end
+  	def hash_keys
+    [ 
+      :nickname, 
+      :phones, 
+      :emails, 
+      :social, 
+      :address, 
+      :legalName, 
+      :cnpj, 
+      :description, 
+      :website
+    ]
+    end
 
-  end
+    def resource_path
+     "#{api_path}/organizations"
+   end
+
+ end
 
 end
--- spec/agendor/organization_spec.rb
+++ spec/agendor/organization_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
-describe "Agendor::Organization", :vcr do
+describe "Agendor::Organization", :vcr => { record: :all } do
 
   it "creates an organization" do
     client = Agendor::Organization.new("brunogh@gmail.com", "qwe123")
-    client.create({:name=>"Whatever", :description=>"Whatever company", :website => "www.whatever.com"}).should eq("201")
+    client.create({:nickname=>"Whatever X", :description=>"Whatever company", :website => "www.whatever.com"}).should eq("201")
   end
 end
