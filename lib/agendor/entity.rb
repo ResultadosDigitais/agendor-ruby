e364a3b39795db73d7486596efdbe7ef68af6a56
Danilo Delponte <danilo.delponte@resultadosdigitais.com.br>
2016-06-09 17:01:08 -0300

    adds update entity

--- lib/agendor/entity.rb
+++ lib/agendor/entity.rb
@@ -18,6 +18,14 @@ module Agendor
       klass_object_id(entity_hash) if entity_hash
     end
 
+    def update(entity_id, params)
+      body = process_hash(params)
+      put = HTTParty.put("#{resource_path}/#{entity_id}",:body => body.to_json, :headers => headers)
+      code = put.response.code
+      raise "Response not HTTP OK: #{code} | #{put.parsed_response["message"]}" if code != "200"
+      klass_object_id(put.parsed_response)
+    end
+
     def process_hash(params)
       params.select {|k, v| hash_keys.include?(k) }
     end
