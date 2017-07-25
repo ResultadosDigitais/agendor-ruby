a6adc33e6f98b435af4660f6dd4a0be0678ca3e9
Bruno Gianni <bcgianni@gmail.com>
2017-07-25 10:01:17 -0300

    remove code climate reporter from spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,9 +3,7 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 
 require 'rspec'
 require 'agendor-ruby'
-require 'codeclimate-test-reporter'
 
-CodeClimate::TestReporter.start
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
